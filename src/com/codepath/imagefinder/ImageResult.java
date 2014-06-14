11b58556dd213a25a380ebe015d35f4384c86ac1
Simon Que <simonque@gmail.com>
2014-06-14 15:26:25 -0700

    Add ImageResult class to store a single image search result

---
+++ src/com/codepath/imagefinder/ImageResult.java
@@ -0,0 +1,31 @@
+package com.codepath.imagefinder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ImageResult {
+	private String url_full;	// URL of image.
+	private String url_thumb;	// URL of thumbnail.
+
+	public ImageResult(JSONObject json) {
+		try {
+			url_full = json.getString("url");
+			url_thumb = json.getString("tbUrl");
+		} catch (JSONException e) {
+			System.err.println(e.getMessage());
+			url_full = null;
+			url_thumb = null;
+		}
+	}
+
+	public String getUrlFull() {
+		return url_full;
+	}
+	public String getUrlThumb() {
+		return url_thumb;
+	}
+
+	public String toString() {
+		return "{ " + url_full + ", " + url_thumb + " }";
+	}
+}
